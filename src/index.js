25dad5f5295eb795b65e719c040de19b5cec3f81
Alan Ray Ampersand <alan@educationsuperhighway.org>
2022-04-19 09:58:21 -0600

    updated packages, and associated config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
