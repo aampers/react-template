9410f9117c89fbeb7e32233220cda9af649ae03c
Alan Ray Ampersand <alan@educationsuperhighway.org>
2022-08-05 13:32:54 -0700

    updated deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App'
+import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+root.render(<App />)
